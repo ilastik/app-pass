9853b76170b80e4edf27cb9e75c1196fab48e258
k-dominik <k-dominik@users.noreply.github.com>
2025-03-04 15:02:04 +0100

    Added barebone cli

--- src/app_pass/__main__.py
+++ src/app_pass/__main__.py
@@ -1,6 +1,38 @@
-def main():
-    print("Implement app_pass's main function!")
+import typer
 
+app = typer.Typer(name="app-pass", no_args_is_help=True)
+
+@app.command()
+def check():
+    """Check if .app bundle is likely to pass MacOs Gatekeeper"""
+    pass
+
+@app.command()
+def fix():
+    """Fix issues in mach-o libraries .app bundle
+
+    Remove paths that point outside the app.
+    """
+    pass
+
+@app.command()
+def sign():
+    """Sign all binaries in the .app bundle"""
+    pass
+
+@app.command()
+def notarize():
+    """Zip app bundle using ditto and send off for notarization"""
+    pass
+
+@app.command()
+def make_pass():
+    """fix, sign, and notarize app"""
+    pass
+
+
+def main():
+    app()
 
 if __name__ == "__main__":
     main()
