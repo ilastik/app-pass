eef8a3a5e134ac9b51286428c7b16a6fadb273d2
k-dominik <k-dominik@users.noreply.github.com>
2025-03-25 14:39:11 +0100

    Pretty summary

--- src/app_pass/__main__.py
+++ src/app_pass/__main__.py
@@ -4,6 +4,8 @@ from typing import Optional, Sequence
 
 import structlog
 import typer
+from rich.console import Console
+from rich.text import Text
 
 from app_pass._app import OSXAPP
 from app_pass._issues import Issue
@@ -28,7 +30,23 @@ def _shared_flags(
 
 
 def print_summary(app: OSXAPP, issues: Sequence[Issue]):
-    print(f"Found {len(issues)} problems with {app.root} of which {len([issue for issue in issues if issue.fixable])} can be fixed.")
+    console = Console()
+    text = Text()
+    text.append(f"Summary for {app.root}\n", style="bold magenta")
+    if not issues:
+        text.append("Found no issues!", style="bold lime")
+    else:
+        text.append(
+            text.assemble(
+                ("Found ", ""),
+                (f"{len(issues)} ", "bold red"),
+                ("issues of which ", ""),
+                (f"{len([issue for issue in issues if issue.fixable])} ", "bold green"),
+                ("can be fixed.", ""),
+            )
+        )
+
+    console.print(text)
 
 @app.command()
 def check(root: Path):
